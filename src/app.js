0283010190f2038f3d1961d0168afff4e1654ab2
Jonas <jonas.backline@gmail.com>
2025-02-23 19:35:18 +0000

    1st commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-import "bootstrap";
-import "./style.css";
+let myDomain = '';
+
+let pronounArray = ['the', 'our', 'my', 'your'];
+let adjArray = ['great', 'big', 'best', 'awesome'];
+let nounArray = ['jogger', 'racoon', 'website', 'space'];
+let domainArray = ['.com', '.net', '.us', '.io']
+
+let pronounRandom = Math.floor(Math.random()*10)*pronounArray.length;
+let adjRandom = Math.floor(Math.random()*10)*adjArray.length;
+let nounRandom = Math.floor(Math.random()*10)*nounArray.length;
+let domainRandom = Math.floor(Math.random()*10)*domainArray.length;
+
+function domainGenerator () {
+  for (let i = 0; i = pronounRandom; i++) {
+    let pronoun = pronounArray[i];
+    for (let i = 0; i = adjRandom; i++) {
+      let adj = adjArray[i];
+      for (let i = 0; i = nounRandom; i++) {
+        let noun = nounArray[i];
+        for (let i = 0; i = domainRandom; i++) {
+          let domain = domainArray[i];
+        }
+      }
+    }
+  }
+    return '${pronoun}${adj}${noun}';
+}
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+console.log(domainGenerator());
